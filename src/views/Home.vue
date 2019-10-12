ebf3526877814916d7c1ec4e1404b27e860541b8
Tilkeii <florian@gouarin.info>
2019-10-13 01:39:14 +0200

    start update getCalendarRawData

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,7 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import { isAuthentificated, retrieveAuth } from "../scripts/storage";
 import { IMyGESAuth } from "../types/auth";
+import { getTime, startOfWeek, endOfWeek, addDays, setHours, setMinutes, setSeconds, setMilliseconds } from "date-fns";
 
 @Component
 export default class Home extends Vue {
@@ -29,12 +30,35 @@ export default class Home extends Vue {
     }
 
     private async getCalendarRawData(auth: IMyGESAuth) {
+
+        /**
+         *  !! A ameliorer voir doc de datefns !!
+         */
+        
+        // const _currentDay = new Date(2019, 9, 24);
+        const _currentDay = new Date();
+        const _startOfWeek = startOfWeek(_currentDay);
+        const _endOfWeek = endOfWeek(_currentDay);
+
+        const _myGesStartOfWeek = setHours(addDays(_startOfWeek, 1), 8);
+        const _myGesEndOfWeek = setMilliseconds(setSeconds(setMinutes(setHours(addDays(_endOfWeek, 1), 8), 0), 0), 0);
+
+        console.log('Current Day', _currentDay);
+        console.log('Start Of week', _startOfWeek);
+        console.log('End Of week', _endOfWeek);
+        console.log('MyGES Start Of week', _myGesStartOfWeek);
+        console.log('MyGES End Of week', _myGesEndOfWeek);
+        console.log('MyGES Start Of week Unix', getTime(_myGesStartOfWeek));
+        console.log('MyGES End Of week Unix', getTime(_myGesEndOfWeek));
+
         let { data } = await this.$http.get<Object>(
             "https://services.reseau-ges.fr/me/agenda",
             {
                 params: {
-                    start: 1568008800000,
-                    end: 1568527200000
+                    // start: 1568008800000,
+                    // end: 1568527200000
+                    start: getTime(_myGesStartOfWeek),
+                    end: getTime(_myGesEndOfWeek)
                 },
                 headers: {
                     Authorization: "Bearer " + auth.token
